0d60d0fc306d911d7426dbf6e15889cab7506a18
vignaruto <atluri_b230857cs@nitc.ac.in>
2026-01-13 23:11:29 +0530

    stage-2 done

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -7,34 +7,42 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
   Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
 
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  cout<< message2<<endl;
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  //ex-1;
-	int j;
-	unsigned char k[101];
-	unsigned char bu[BLOCK_SIZE];
-	Disk::readBlock(bu,0);
-	memcpy(k,bu,100);
-	for(int i=0;i<15;i++){
-		j=(int)k[i];
-		cout<<j<<" ";
-	}
-	cout<<endl;
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i=0;i<relCatHeader.numEntries;i++) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (int j=0;j<attrCatHeader.numEntries;j++) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+      attrCatBuffer.getRecord(attrCatRecord,j);
+
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal)==0) {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+    printf("\n");
+  }
 
   return 0;
-  //return FrontendInterface::handleFrontend(argc, argv);
 }
