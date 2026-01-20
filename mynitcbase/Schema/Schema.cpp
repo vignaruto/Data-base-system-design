a9d8777d61a3a4ce9dd6148cce8a055ee8b5e9f5
vignaruto <atluri_b230857cs@nitc.ac.in>
2026-01-20 09:17:49 +0530

    stage-6 done

--- mynitcbase/Schema/Schema.cpp
+++ mynitcbase/Schema/Schema.cpp
@@ -2,18 +2,22 @@
 
 #include <bits/stdc++.h>
 
-int Schema::openRel(char relName[ATTR_SIZE]) {
-  int ret = OpenRelTable::openRel(relName);
 
-  // the OpenRelTable::openRel() function returns the rel-id if successful
-  // a valid rel-id will be within the range 0 <= relId < MAX_OPEN and any
-  // error codes will be negative
-  if(ret >= 0 && ret<MAX_OPEN){
-    return SUCCESS;
-  }
 
-  //otherwise it returns an error message
-  return ret;
+int Schema::openRel(char relName[ATTR_SIZE])
+{
+    int ret = OpenRelTable::openRel(relName);
+
+    // the OpenRelTable::openRel() function returns the rel-id if successful
+    // a valid rel-id will be within the range 0 <= relId < MAX_OPEN and any
+    // error codes will be negative
+    if (ret >= 0 && ret < MAX_OPEN)
+    {
+        return SUCCESS;
+    }
+
+    // otherwise it returns an error message
+    return ret;
 }
 
 int Schema::closeRel(char relName[ATTR_SIZE]) {
@@ -32,3 +36,52 @@ int Schema::closeRel(char relName[ATTR_SIZE]) {
   return OpenRelTable::closeRel(relId);
 }
 
+int Schema::renameRel(char oldRelName[ATTR_SIZE], char newRelName[ATTR_SIZE])
+{
+    // if the oldRelName or newRelName is either Relation Catalog or Attribute Catalog,
+        // return E_NOTPERMITTED
+        // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
+        // you may use the following constants: RELCAT_RELNAME and ATTRCAT_RELNAME)
+      if(strcmp(oldRelName,(char *)RELCAT_RELNAME)==0 || 
+         strcmp(oldRelName,(char *)ATTRCAT_RELNAME)==0 ||
+         strcmp(newRelName,(char *)RELCAT_RELNAME)==0 ||
+         strcmp(newRelName,(char *)ATTRCAT_RELNAME)==0){
+          return E_NOTPERMITTED;
+         }
+
+    // if the relation is open
+    //    (check if OpenRelTable::getRelId() returns E_RELNOTOPEN)
+    //    return E_RELOPEN
+    if(OpenRelTable::getRelId(oldRelName)!=E_RELNOTOPEN){
+      return E_RELOPEN;
+    }
+
+    // retVal = BlockAccess::renameRelation(oldRelName, newRelName);
+    // return retVal
+    int retval=BlockAccess::renameRelation(oldRelName,newRelName);
+    return retval;
+}
+
+int Schema::renameAttr(char relName[ATTR_SIZE], char oldAttrName[ATTR_SIZE], char newAttrName[ATTR_SIZE])
+{
+    // if the relName is either Relation Catalog or Attribute Catalog,
+        // return E_NOTPERMITTED
+        // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
+        // you may use the following constants: RELCAT_RELNAME and ATTRCAT_RELNAME)
+      if(strcmp(relName,(char *)RELCAT_RELNAME)==0 || strcmp(relName,(char *)ATTRCAT_RELNAME)==0){
+        return E_NOTPERMITTED;
+      }
+
+    // if the relation is open
+        //    (check if OpenRelTable::getRelId() returns E_RELNOTOPEN)
+        //    return E_RELOPEN
+      if(OpenRelTable::getRelId(relName)!=E_RELNOTOPEN){
+        return E_RELOPEN;
+      }
+
+    // Call BlockAccess::renameAttribute with appropriate arguments.
+    int retval=BlockAccess::renameAttribute(relName,oldAttrName,newAttrName);
+    return retval;
+
+    // return the value returned by the above renameAttribute() call
+}
